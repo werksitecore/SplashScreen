724c02268e45768b28737519e20a4931af442c4a
Sanal Menon <smk@menon.me>
2015-02-05 09:27:07 +0400

    Added Fade Effect

--- Scripts/jquery.SplashScreen.js
+++ Scripts/jquery.SplashScreen.js
@@ -1,13 +1,16 @@
 /*!
  * jQuery Splash Screen plugin
- * Version 1.00.0-2015.02.03
+ * Version 1.00.1-2015.02.03
  * Requires jQuery
  *
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
+ *
+ * Contributions:
+ *       -  Simple Fade Animation by 'Michael Kuehl' (http://tech.pro/tutorial/725/javascript-tutorial-simple-fade-animation)
+ *
  */
-
 
 (function (window, document, undefined) {
 
@@ -23,7 +26,10 @@
             progressCountId: 'status',
             progressBar: false,
             progressBarId: 'progress',
+            fadeEffect: true,
+            timeToFade: 1000, // in milliseconds (eg: 1000 = 1sec)
             timeOut: 2000   // in milliseconds (eg: 2000 = 2sec)
+
         }, options);
 
         function id(v) { return document.getElementById(v); }
@@ -71,8 +77,14 @@
                 }
 
                 function pageLoadCompleted() {
-                    overly.style.opacity = 0;
-                    overly.style.display = "none";
+
+                    if (settings.fadeEffect)
+                        fade(settings.id);
+                    else {
+                        overly.style.opacity = 0;
+                        overly.style.display = "none";
+                    }
+                    
                 }
 
                 for (var i = 0; i < totalImages; i++) {
@@ -96,6 +108,54 @@
                 vars[hash[0]] = hash[1];
             }
             return vars;
+        }
+
+        function animateFade(lastTick, eid) {
+            var curTick = new Date().getTime();
+            var elapsedTicks = curTick - lastTick;
+
+            var element = document.getElementById(eid);
+
+            if (element.FadeTimeLeft <= elapsedTicks) {
+                element.style.opacity = element.FadeState == 1 ? '1' : '0';
+                element.style.filter = 'alpha(opacity = ' + (element.FadeState == 1 ? '100' : '0') + ')';
+                element.FadeState = element.FadeState == 1 ? 2 : -2;
+                return;
+            }
+
+            element.FadeTimeLeft -= elapsedTicks;
+            var newOpVal = element.FadeTimeLeft / settings.timeToFade;
+            if (element.FadeState == 1)
+                newOpVal = 1 - newOpVal;
+
+            element.style.opacity = newOpVal;
+            element.style.filter = 'alpha(opacity = ' + (newOpVal * 100) + ')';
+
+            setTimeout(function () { animateFade(curTick, eid) }, 33);
+        }
+
+
+        function fade(eid) {
+            var element = document.getElementById(eid);
+
+
+            if (element.FadeState == null) {
+                if (element.style.opacity == null || element.style.opacity == ''
+                   || element.style.opacity == '1')
+                    element.FadeState = 2;
+                else
+                    element.FadeState = -2;
+            }
+
+            if (element.FadeState == 1 || element.FadeState == -1) {
+                element.FadeState = element.FadeState == 1 ? -1 : 1;
+                element.FadeTimeLeft = settings.timeToFade - element.FadeTimeLeft;
+            }
+            else {
+                element.FadeState = element.FadeState == 2 ? -1 : 1;
+                element.FadeTimeLeft = settings.timeToFade;
+                setTimeout(animateFade(new Date().getTime(), eid), 33);
+            }
         }
 
         try {
